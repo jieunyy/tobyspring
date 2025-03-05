a1196396f8706db9c30b25901f32b71e87cb63b0
jieunyy <ina011011@naver.com>
2025-03-05 19:35:07 +0900

    feat: Servlet Controller Mapping & Binding

--- src/main/java/lucyspring/helloboot/HellobootApplication.java
+++ src/main/java/lucyspring/helloboot/HellobootApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,18 +25,33 @@ public class HellobootApplication {
 		//SpringApplication.run(HellobootApplication.class, args);
 		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			HelloController helloController = new HelloController();
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					///  요청 객체 사용
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 기능
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						///  요청 객체 사용
+						String name = req.getParameter("name");
 
-					/// 응답 객체
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello "+ name);
+						String ret = helloController.hello(name);
+
+						/// 응답 객체
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					}
+
+					else if (req.getRequestURI().equals("/user")){
+						//
+					}
+
+					else {
+						resp.setStatus((HttpStatus.NOT_FOUND.value()));
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*"); // 모든 요청 처리(프론트 컨트롤러)
 		});
 		webServer.start();
 	}
